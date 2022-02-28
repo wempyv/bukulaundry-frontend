4e65dedfe7f10174ca2dd0d7882fbfc57e6fb175
wempyv <wempyv@gmail.com>
2022-03-01 00:24:20 +0700

    📦 NEW

---
+++ pages/transactions/detail-transaction.js
@@ -0,0 +1,130 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+import AdminLayout from "../../components/layout/AdminLayout";
+import ModalAddItem from "../../components/component/ModalAddItem";
+
+const AddTransaction = () => {
+  const [showModal, setShowModal] = useState(false);
+  const router = useRouter();
+  return (
+    <AdminLayout>
+      <div className="flex flex-col w-full md:ml-4 px-2 items-stretch">
+        <section>
+          <h1 className="text-2xl font-medium">Detail Transaksi</h1>
+          <nav className="flex mt-10 mb-4" aria-label="Breadcrumb">
+            <ol className="inline-flex items-center space-x-1 md:space-x-3">
+              <li>
+                <div className="flex items-center">
+                  <a
+                    onClick={() => router.push("/transactions")}
+                    className="text-sm text-blue-600 hover:text-blue-900 cursor-pointer "
+                  >
+                    Data Transaksi
+                  </a>
+                </div>
+              </li>
+              <li aria-current="page">
+                <div className="flex items-center">
+                  <svg
+                    className="w-6 h-6 text-[#232020]"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  <span className="ml-1 text-sm  text-[#232020] md:ml-2 dark:text-gray-500">
+                    Detail Transaksi
+                  </span>
+                </div>
+              </li>
+            </ol>
+          </nav>
+        </section>
+        <section>
+          <div className="md:flex">
+            <div className="md:w-4/12">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/4/43/WhatsApp_click-to-chat_QR_code.png"
+                className="h-72"
+              />
+            </div>
+            <div className="md:w-4/12 flex flex-col md:mx-4">
+              <h1 className="text-2xl font-semibold text-[#232020]">
+                INVOICE#12378
+              </h1>
+              <div className="form-group flex flex-col mt-4 mb-4">
+                <span className="text-sm text-[#B89F9F]">Nama Customer</span>
+                <span className="text-[#232020] font-medium">
+                  Wempy Virgana
+                </span>
+              </div>
+              <div className="form-group flex flex-col mb-4">
+                <span className="text-sm text-[#B89F9F]">Total Berat</span>
+                <span className="text-[#232020] font-medium">20Kg</span>
+              </div>
+              <div className="form-group flex flex-col mb-4">
+                <span className="text-sm text-[#B89F9F]">
+                  Tanggal Transaksi
+                </span>
+                <span className="text-[#232020] font-medium">
+                  6 Januari 2022
+                </span>
+              </div>
+            </div>
+            <div className="md:w-4/12 flex flex-col md:mx-4 md:mt-8">
+              <div className="form-group flex flex-col mt-4 mb-4">
+                <span className="text-sm text-[#B89F9F]">Nomor Whatsapp</span>
+                <span className="text-[#232020] font-medium">087742781165</span>
+              </div>
+              <div className="form-group flex flex-col mb-4">
+                <span className="text-sm text-[#B89F9F]">Total Tagihan</span>
+                <span className="text-[#232020] font-medium">Rp120.000</span>
+              </div>
+              <div className="form-group flex flex-col mb-4">
+                <span className="text-sm text-[#B89F9F]">
+                  Status Pembayaran
+                </span>
+                <span className="text-[#232020] font-medium">Belum Bayar</span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="md:flex">
+            <div className="md:w-3/12 flex flex-col">
+              <div className="form-group flex flex-col mt-4 mb-4">
+                <span className="text-sm text-[#B89F9F]">Alamat Customer</span>
+                <span className="text-[#232020] font-medium">
+                  Jalan Gatot Subroto no 02 tanjung pandan belitung
+                </span>
+              </div>
+              <div className="form-group flex flex-col mb-4">
+                <span className="text-sm text-[#B89F9F]">
+                  Status proses laundry
+                </span>
+                <span className="text-[#232020] font-medium">
+                  Finish(Sudah selesai)
+                </span>
+              </div>
+            </div>
+            <div className="md:w-3/12 flex flex-col md:mx-4 ">
+              <div className="form-group flex flex-col mt-4 mb-4">
+                <span className="text-sm text-[#B89F9F]">Status on-demand</span>
+                <span className="text-[#232020] font-medium">
+                  Sedang di antar
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </AdminLayout>
+  );
+};
+
+export default AddTransaction;
--- pages/transactions/index.js
+++ pages/transactions/index.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { useRouter } from "next/router";
 import TableTransaction from "../../components/component/TableTransaction";
 import AdminLayout from "../../components/layout/AdminLayout";
 
 const index = () => {
+  const router = useRouter();
   return (
     <AdminLayout>
       <div className="flex flex-col w-full md:ml-4 px-2 items-stretch">
         <section>
           <div className="flex md:flex-row flex-col">
             <h1 className="text-2xl font-medium">Data Transaksi</h1>
-            <button className="rounded w-full md:w-[18rem] h-[2.8rem] bg-gray-900 text-white text-sm mt-6 md:my-0  hover:scale-105 hover:shadow-xl duration-300 ease-in-out ml-auto font-medium">
+            <button
+              className="rounded w-full md:w-[18rem] h-[2.8rem] bg-gray-900 text-white text-sm mt-6 md:my-0  hover:scale-105 hover:shadow-xl duration-300 ease-in-out ml-auto font-medium"
+              onClick={() => router.push("/transactions/add-transaction")}
+            >
               + Tambah Transaksi
             </button>
           </div>
