25220421734f808bcbd274006be5bdf963b205d5
wempyv <wempyv@gmail.com>
2022-03-31 16:22:45 +0700

    🐛 FIX: A BUG

--- components/layout/AuthLayout.js
+++ components/layout/AuthLayout.js
@@ -5,7 +5,7 @@ const AuthLayout = ({ children }) => {
   return (
     <div className="px-8 md:px-24 py-7 min-h-screen">
       <section className="header">
-        <Link href="/">
+        <Link href="/auth">
           <img
             src="/assets/logo.svg"
             className="w-28 object-cover cursor-pointer"
