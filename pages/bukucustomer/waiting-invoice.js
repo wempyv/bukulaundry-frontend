fc5edd9d860b7fa3a46c125a1861cc848791ccff
wempyv <wempyv@gmail.com>
2022-04-01 21:24:55 +0700

    👌 IMPROVE

--- pages/bukucustomer/waiting-invoice.js
+++ pages/bukucustomer/waiting-invoice.js
@@ -8,7 +8,7 @@ const AddTransactionFromCustomer = () => {
             <h1 className="text-2xl font-semibold mt-2 text-[#232020]">Sukses!</h1>
             <p className="text-center w-80 text-gray-400 mt-2">Transaksi telah diterima & Invoice kamu akan segera dikirim 🚀</p>
             <button onClick={() => router.push('/bukucustomer/check-invoice')}
-                className="rounded w-3/5 mx-auto hover:scale-105  h-[2.8rem] bg-[#232020] text-white text-sm mt-10 hover:bg-[#111010] hover:shadow-xl duration-300 ease-in-out font-medium ml-auto flex items-center justify-center"
+                className="rounded w-4/5 md:w-3/5 mx-auto hover:scale-105  h-[2.8rem] bg-[#232020] text-white text-sm mt-10 hover:bg-[#111010] hover:shadow-xl duration-300 ease-in-out font-medium ml-auto flex items-center justify-center"
             >
                 Halaman Invoice
             </button>
