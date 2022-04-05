48594daf11c36f1da16d9951d849401b9f059681
wempyv <wempyv@gmail.com>
2022-04-05 13:56:27 +0700

    👌 IMPROVE: REFACTORING

--- pages/settings.js
+++ pages/settings.js
@@ -74,7 +74,7 @@ const index = () => {
       <div className="flex flex-col w-full md:ml-4 px-2 items-stretch">
         <section>
           <h1 className="text-2xl font-medium">Pengaturan akun</h1>
-          <p className="mt-5 flex items-center text-sm">URL Transaksi customer : <span className="text-blue-600 mx-2">{idLaundry}</span> <CopyToClipboard
+          <p className="mt-5 flex items-center text-sm">URL Transaksi customer : <span className="text-blue-600 mx-2">{userToken.idLaundry}</span> <CopyToClipboard
             text={`http://localhost:3000/bukucustomer/add-transaction/${idLaundry}`}
             onCopy={() => toast.success('URL Berhasil disalin')}>
             <button className="hover:scale-110"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
